bf2e91d7d0151b30cf7b06d4255c34246dad1d78
Ihor Khomenko <igorhomenko97@gmail.com>
2022-07-27 17:14:21 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,45 +13,44 @@ class App extends Component {
     {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
     filter: '',
     name: '',
-    number: ''
+    number: '',
+    good: 0,
+    neutral: 0,
+    bad: 0,
   };
-  // state = {
-  //   good: 0,
-  //   neutral: 0,
-  //   bad: 0,
-  // };
+  
 
-  // handleBtn = e => {
+  handleBtn = e => {
 
-  //   if (e.target.textContent === 'good') {
-  //     console.log(this.state.good)
-  //     this.setState(prevState => ({
-  //       good: prevState.good + 1,
-  //     }));
-  //   }
-  //   if (e.target.textContent === 'neutral') {
-  //     console.log(this.state.neutral)
-  //     this.setState(prevState => ({
-  //       neutral: prevState.neutral + 1,
-  //     }));
-  //   }
-  //   if (e.target.textContent === 'bad') {
-  //     console.log(this.state.bad)
-  //     this.setState(prevState => ({
-  //       bad: prevState.bad + 1,
-  //     }));
-  //   }
-  // };
-  // handleNeutral = () => {
-  //   this.setState(prevState => ({
-  //     neutral: prevState.neutral + 1,
-  //   }));
-  // };
-  // handleBad = () => {
-  //   this.setState(prevState => ({
-  //     bad: prevState.bad + 1,
-  //   }));
-  // };
+    if (e.target.textContent === 'good') {
+      console.log(this.state.good)
+      this.setState(prevState => ({
+        good: prevState.good + 1,
+      }));
+    }
+    if (e.target.textContent === 'neutral') {
+      console.log(this.state.neutral)
+      this.setState(prevState => ({
+        neutral: prevState.neutral + 1,
+      }));
+    }
+    if (e.target.textContent === 'bad') {
+      console.log(this.state.bad)
+      this.setState(prevState => ({
+        bad: prevState.bad + 1,
+      }));
+    }
+  };
+  handleNeutral = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
+  };
+  handleBad = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
+  };
 
   handleSubmitForm = data => {
     this.setState({contacts: [...this.state.contacts, data]})
@@ -68,13 +67,40 @@ class App extends Component {
 
   render() {
     
-    // const { good, neutral, bad } = this.state;
-    // const total = good + neutral + bad;
-    // const positiveFeedback = Math.trunc((good / (good + neutral + bad)) * 100);
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    const positiveFeedback = Math.trunc((good / (good + neutral + bad)) * 100);
     const normalazeFilter = this.state.filter.toLowerCase()
     const visibleContacts = this.state.contacts.filter(contact => contact.name.toLowerCase().includes(normalazeFilter))
     return (
       <div>
+        <section>
+        {/* <div>
+          <h2>Please leave feedback</h2>
+          <button type="button" onClick={this.handleGood}>
+            good
+          </button>
+          <button type="button" onClick={this.handleNeutral}>
+            neutral
+          </button>
+          <button type="button" onClick={this.handleBad}>
+            bad
+          </button>
+        </div> */}
+        <div className='btns'>
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={this.handleBtn}
+        />
+        </div>
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total}
+          positiveFeedback={good === 0 ? 0 : positiveFeedback}
+        />
+      </section>
         <section>
           <PhoneRegBook onSubmit={this.handleSubmitForm}/>
         </section>
@@ -84,33 +110,7 @@ class App extends Component {
           <Contacts contacts={visibleContacts} onDelete={this.handleDelete}/>
         </section>
       </div>
-      // <section>
-      //   {/* <div>
-      //     <h2>Please leave feedback</h2>
-      //     <button type="button" onClick={this.handleGood}>
-      //       good
-      //     </button>
-      //     <button type="button" onClick={this.handleNeutral}>
-      //       neutral
-      //     </button>
-      //     <button type="button" onClick={this.handleBad}>
-      //       bad
-      //     </button>
-      //   </div> */}
-      //   <div className='btns'>
-      //   <FeedbackOptions
-      //     options={['good', 'neutral', 'bad']}
-      //     onLeaveFeedback={this.handleBtn}
-      //   />
-      //   </div>
-      //   <Statistics
-      //     good={good}
-      //     neutral={neutral}
-      //     bad={bad}
-      //     total={total}
-      //     positiveFeedback={good === 0 ? 0 : positiveFeedback}
-      //   />
-      // </section>
+      
     );
   }
 }
